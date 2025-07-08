429ea92277165aef7be5aa04254659a64590177e
Kamdem-cmd <=>
2025-07-08 07:54:38 +0100

    upload files

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-let textTag = document.querySelectorAll(".text");
-let bgInput = document.querySelector("#bgInput");
-let bgColor = document.body;
 
-bgColor.style.backgroundColor = `${bgInput}.value`;
+document.addEventListener('DOMContentLoaded', () => {
+    let textTag = document.querySelectorAll(".text");
+    let bgInput = document.querySelector("#bgInput");
+    let bgColor = document.body;
 
-console.log(`bgInput: ${bgInput}.value`);
+    // la fonction pour changer la couleur de fond
+    function ChangeBg() {
+        // La valeur de l'input de type couleur est une chaîne hexadécimale (ex: #RRGGBB)
+        // Utilisez cette valeur directement pour la couleur de fond
+        bgColor.style.backgroundColor = bgInput.value;
+        console.log(`Couleur sélectionnée: ${bgInput.value}`);
+    }
+
+    bgInput.addEventListener("input", ChangeBg);
+});
